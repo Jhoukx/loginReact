7605c052ec86cb91ca8498b486f9b79a70b9b8e8
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-21 17:53:35 -0500

    build: :fire: Delete logon and registration to centralize it in one route

--- app/routes/login.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const appLogin = Router();
-
-appLogin.get('/', (req, res) => {
-    res.send({message:"All good :D"});
-});
-
-export default appLogin;
--- app/routes/register.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const appRegister = Router();
-
-appRegister.post('/', (req, res) => {
-    res.send('Registering...');
-})
-
-export default appRegister;
