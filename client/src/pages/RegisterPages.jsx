c0f73631d17e24cde58b6173808c7ad2b7133d50
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-21 16:41:31 -0500

    feat: :sparkles: Form inputs fot register path

--- client/src/pages/RegisterPages.jsx
+++ client/src/pages/RegisterPages.jsx
@@ -1,7 +1,41 @@
+import {useForm} from 'react-hook-form'
 function RegisterPages() {
+  const { register,handleSubmit } = useForm();
+
   return (
-    <div>RegisterPages</div>
-  )
+    <>
+      <h1>Register</h1>
+      <form
+        onSubmit={handleSubmit((values) => {
+          console.log(values);
+        })}
+      >
+        <input
+          type="text"
+          placeholder="Username"
+          {...register("username", { required: true })}
+        />{" "}
+        <br />
+        <br />
+        <input
+          type="text"
+          placeholder="Email"
+          {...register("email", { required: true })}
+        />{" "}
+        <br />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password", { required: true })}
+        />
+        <br />
+        <br />
+        <button type="submit">Register</button>
+        <p>Already have a account? <a href="/login">Log in </a></p>
+      </form>
+    </>
+  );
 }
 
 export default RegisterPages
