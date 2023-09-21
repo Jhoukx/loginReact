db09b5717047cf88e97896921326be3ade083ebd
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-21 18:03:51 -0500

    refactor: :recycle: Rewrite mongodb connection

--- app/connection/connection.js
+++ app/connection/connection.js
@@ -1,14 +1,10 @@
 import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
 dotenv.config('../');
-export async function con() {
+export async function conexion() {
     try {
         const uri = `mongodb+srv://root:${process.env.ATLAS_PASSWORD}@cluster0.hiif8hy.mongodb.net/${process.env.ATLAS_DB}`
-        const options = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        };
-        const client = await MongoClient.connect(uri, options);
+        const client = await MongoClient.connect(uri);
         return client.db();
     } catch (error) {
         return { status: 500, message: error.message }
