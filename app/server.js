ab1c0c3d9b0129484225904a7f031538014f3cf6
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-21 17:41:17 -0500

    feat: :sparkles: Use cors dependency

--- app/server.js
+++ app/server.js
@@ -2,10 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import appLogin from './routes/login.routes.js';
 import appRegister from './routes/register.routes.js';
+import cors from 'cors';
+
 dotenv.config();
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 
 server.use('/login', appLogin);
