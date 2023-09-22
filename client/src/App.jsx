97836cc540c42263271c191cc0f3dbb6b0b3c7f2
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-22 12:47:15 -0500

    feat: :sparkles: Use AuthProvider and redirect to Profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
+import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import './App.css'
 import RegisterPages from './pages/RegisterPages';
+import Profile from './pages/Profile';
+import { AuthProvider } from './context/authContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home page</h1>} />
-        <Route path="/account/login" element={<LoginPage />} />
-        <Route path="/account/register" element={<RegisterPages/>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="/account/login" element={<LoginPage />} />
+          <Route path="/account/register" element={<RegisterPages />} />
+          <Route path='/profile' element={ <Profile/>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
