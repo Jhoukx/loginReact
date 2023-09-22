840ff9a5daf91c4890323252c9713e5afb8a90dc
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-22 09:30:09 -0500

    feat: :sparkles: Add register logic

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,14 +1,22 @@
 import { Router } from "express";
 import { collectionGen } from "../connection/connection.js";
 
-const user = await collectionGen("users");
+const user = await collectionGen("User");
 const appUser = Router();
 
 appUser.post('/login', async (req, res) => {
     res.send('Login successful');
 });
 appUser.post('/register', async (req, res) => {
-    res.send('Register');
+    try {
+        let response = await user.findOne({ email: req.body.email });
+        if (response) return res.status(404).json({ status: 404, message: "Email is not available" })
+        const inserUser = await user.insertOne(req.body);
+        res.json({status:200,message:"User created successfully"})
+    } catch (error) {
+        console.log(error)
+        res.send()
+    }
 });
 
 
