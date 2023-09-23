6899054205f78add05a4e6eced6e399bc07772d4
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-22 22:27:20 -0500

    feat: :sparkles: loginRequest

--- client/api/auth.js
+++ client/api/auth.js
@@ -2,4 +2,6 @@ import axios from 'axios';
 
 const API = "http://127.0.0.1:4017"
 
-export const registerRequest = user => axios.post(`${API}/account/register`, user)
+export const registerRequest = user => axios.post(`${API}/account/register`, user);
+
+export const loginRequest = user => axios.post(`${API}/account/login`,user);
