a760f3afc82c0fc38bd2211d454ab7066336a6db
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-09-25 15:50:41 -0500

    feat: :sparkles: add username to profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,10 @@
+import { useAuth } from "../context/authContext";
+
 function Profile() {
+    const { user } = useAuth();
     return (
         <>
-            <h1>Hello User :D</h1>
+            <h1>Hello {user.user.username} We are happy to see you again :D</h1>
         </>
     )
 }
